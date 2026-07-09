ef915f9c072cd5a30ba5307b9bb4cc9b0f33abf2
arrayforward <13581828915@139.com>
2026-07-09 10:52:33 +0800

    Add files via upload

---
+++ vendor/miniz_export.h
@@ -0,0 +1,6 @@
+// Minimal stub for miniz_export.h - the official file was missing from upstream.
+// We only do static linking, so make MINIZ_EXPORT expand to nothing.
+#ifndef MINIZ_EXPORT_H
+#define MINIZ_EXPORT_H
+#define MINIZ_EXPORT
+#endif
